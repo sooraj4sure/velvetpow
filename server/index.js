62ff70e25f19701c96d59d5c4d8344a0eb46c77c
sooraj4sure <skashyap9711@gmail.com>
2026-04-12 21:45:44 +0530

    vercel link added to server

--- server/index.js
+++ server/index.js
@@ -5,8 +5,10 @@ const cors     = require('cors');
 const path     = require('path');
 
 const app = express();
-
-app.use(cors({ origin: 'http://localhost:5173' }));  // your Vite frontend
+app.get('/', (req, res) => {
+  res.send('Backend is running 🚀');
+});
+app.use(cors({ origin: ['https://velvetpow.vercel.app','http://localhost:5173'],credentials: true }));  // your Vite frontend
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
